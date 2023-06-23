a80734a33a1a81b306cf7187375a89ce98d7380d
Class01 <Class01@DESKTOP-9H6B9C5>
2023-06-23 17:14:39 +0900

    add the select item.

--- JavaSwing/src/jtable/test/Main.java
+++ JavaSwing/src/jtable/test/Main.java
@@ -2,6 +2,7 @@ package jtable.test;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Vector;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -12,7 +13,7 @@ import javax.swing.table.DefaultTableModel;
 public class Main implements MouseListener {
 	private JFrame frame;
 	private JTable table;
-	
+
 	public Main() {
 		frame = new JFrame("JTable...");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,46 +25,52 @@ public class Main implements MouseListener {
 		table = new JTable(model);
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.addMouseListener(this);
-		
+
 		JScrollPane scrollPane = new JScrollPane(table);
 
 		frame.add(scrollPane);
 		frame.pack();
 		frame.setVisible(true);
 	}
-	
+
 	public static void main(String[] args) {
 		new Main();
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		System.out.println(table.getSelectedRow() + ":" +  table.getSelectedColumn());
+		System.out.println(table.getSelectedRow() + ":" + table.getSelectedColumn());
 		System.out.println(table.getValueAt(table.getSelectedRow(), table.getSelectedColumn()));
+
+		Object[] addo = new Object[3];
+		for (int i = 0; i < addo.length; i++) {
+			addo[i] = table.getValueAt(table.getSelectedRow(), i);
+		}
+		((DefaultTableModel) table.getModel()).addRow(addo);
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
